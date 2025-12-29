69a5e6433d5ae080c961c640895f68475dd4c773
Simon Cropp <simon.cropp@gmail.com>
2025-12-29 20:25:31 +1100

    .

--- tests/MermaidSharp.Tests/SvgVerify.cs
+++ tests/MermaidSharp.Tests/SvgVerify.cs
@@ -2,6 +2,15 @@
 
 public class TestBase: PageTest
 {
+    // Set device scale factor for higher resolution screenshots
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions
+        {
+            DeviceScaleFactor = 2
+        };
+    }
+
     public async Task VerifySvg(
         string input,
         [CallerFilePath] string sourceFile = "")
@@ -15,20 +24,23 @@ public class TestBase: PageTest
     async Task<MemoryStream> ConvertSvgToPngAsync(string svgContent)
     {
         // Create an HTML page with the SVG
-        var html = $@"<!DOCTYPE html>
-<html>
-<head>
-    <meta charset=""UTF-8"">
-    <style>
-        * {{ margin: 0; padding: 0; }}
-        body {{ background: white; display: inline-block; }}
-    </style>
-    <link rel=""stylesheet"" href=""https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"">
-</head>
-<body>
-{svgContent}
-</body>
-</html>";
+        var html =
+            $$"""
+              <!DOCTYPE html>
+              <html>
+              <head>
+                  <meta charset="UTF-8">
+                  <style>
+                      * { margin: 0; padding: 0; }
+                      body { background: white; display: inline-block; }
+                  </style>
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+              </head>
+              <body>
+              {{svgContent}}
+              </body>
+              </html>
+              """;
 
         await Page.SetContentAsync(html, new() {WaitUntil = WaitUntilState.NetworkIdle});
 
